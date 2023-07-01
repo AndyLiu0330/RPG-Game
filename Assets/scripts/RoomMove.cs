8b5202fe76ab5d391f513b9136d789042fe68c14
AndyLiu0330 <andyliu0330@gmail.com>
2023-06-30 20:44:08 -0700

    dialog
    put the dialog of the sign and the pop out sign.

--- Assets/scripts/RoomMove.cs
+++ Assets/scripts/RoomMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomMove : MonoBehaviour
 {
@@ -9,6 +10,10 @@ public class RoomMove : MonoBehaviour
     
     public Vector3 playerChange;
     private CameraMovement cam;
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
     void Start()
     {
         cam = Camera.main.GetComponent<CameraMovement>();
@@ -27,5 +32,15 @@ public class RoomMove : MonoBehaviour
             other.transform.position += playerChange;
 
         }
+        if (needText){
+            StartCoroutine(placeNameCo());
+
+        }
+    }
+    private IEnumerator placeNameCo (){
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(4f);
+        text.SetActive(false);
     }
 }
