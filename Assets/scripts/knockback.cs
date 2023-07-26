faf034d0cbbfb725f9b313e4b6502e44aa823c31
AndyLiu0330 <andyliu0330@gmail.com>
2023-07-26 15:33:14 +0800

    part 21

--- Assets/scripts/knockback.cs
+++ Assets/scripts/knockback.cs
@@ -6,7 +6,7 @@ public class knockback : MonoBehaviour
 {
     public float thrust;
     public float koncktime;
-
+    public float damage;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -14,7 +14,7 @@ public class knockback : MonoBehaviour
         {
             other.GetComponent<pot>().Smash();
         }
-        if (other.gameObject.CompareTag("enemy" ) || other.gameObject.CompareTag("Player"));  
+        if (other.gameObject.CompareTag("enemy") || other.gameObject.CompareTag("Player")) ;
         {
             Rigidbody2D hit = other.GetComponent<Rigidbody2D>();
             if (hit != null)
@@ -22,31 +22,32 @@ public class knockback : MonoBehaviour
                 Vector2 differece = hit.transform.position - transform.position;
                 differece = differece.normalized * thrust;
                 hit.AddForce(differece, ForceMode2D.Impulse);
-                if (other.gameObject.CompareTag("enemy"))
+                if (other.gameObject.CompareTag("enemy") && other.isTrigger)
                 {
                     hit.GetComponent<Enemy>().currentState = EnemyState.stagger;
-                    other.GetComponent<Enemy>().Knock(hit, koncktime);
+                    other.GetComponent<Enemy>().Knock(hit, koncktime, damage);
                 }
                 if (other.gameObject.CompareTag("Player"))
                 {
                     hit.GetComponent<playermovement>().currentState = PlayerState.stagger;
                     other.GetComponent<playermovement>().Knock(koncktime);
                 }
-        
+
                 // StartCoroutine(KnockCO(hit));
+
+
             }
 
         }
+        // private IEnumerator KnockCO(Rigidbody2D enemy)
+        // {
+        //     if (enemy != null)
+        //     {
+        //         yield return new WaitForSeconds(koncktime);
+        //         enemy.velocity = Vector2.zero;
+        //         enemy.GetComponent<Enemy>().currentState = EnemyState.idle;
+        //     }
+        // }
 
     }
-    // private IEnumerator KnockCO(Rigidbody2D enemy)
-    // {
-    //     if (enemy != null)
-    //     {
-    //         yield return new WaitForSeconds(koncktime);
-    //         enemy.velocity = Vector2.zero;
-    //         enemy.GetComponent<Enemy>().currentState = EnemyState.idle;
-    //     }
-    // }
-
 }
